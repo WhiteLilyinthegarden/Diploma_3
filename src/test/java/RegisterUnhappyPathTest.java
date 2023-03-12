fffdf59673cb2509847a26166eaf9bb6a5cac808
White Lily <lili007white@gmail.com>
2023-03-12 11:30:13 +0300

    corrected the test

--- src/test/java/RegisterUnhappyPathTest.java
+++ src/test/java/RegisterUnhappyPathTest.java
@@ -3,6 +3,7 @@ import client.UserRequest;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.restassured.response.ValidatableResponse;
 import model.User;
+import org.openqa.selenium.chrome.ChromeOptions;
 import pageobject.RegisterPage;
 import io.qameta.allure.junit4.DisplayName;
 import org.junit.After;
@@ -12,36 +13,48 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+
 import static org.junit.Assert.assertTrue;
 
 public class RegisterUnhappyPathTest {
     private WebDriver driver;
     private RegisterPage registerPage;
     private UserRequest userRequest;
-
     User user = new User();
+    String passwordLess6Char = "P@ss";
 
-   @Before
-  public void testCreateOrder() {
+    @Before
+    public void testCreateOrder() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
         userRequest = new UserRequest();
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.get(RestClient.BASE_URL);
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get(RestClient.BASE_URL);
         registerPage = new RegisterPage(driver);
-        userRequest.create(user);
-        }
+    }
+
     @Test
     @DisplayName("Неккоректный пароль при Регистрации меннее 6 символов")
     public void registrationFail() {
+        user.setPassword(passwordLess6Char);
         registerPage.registrationFailed(
                 user.getName(),
                 user.getEmail(),
-                "P@ss");
+                user.getPassword());
         assertTrue(registerPage.getPasswordError());
     }
+
     @After
     public void tearDown() {
+        try {
+            ValidatableResponse response = userRequest.login(user);
+            String accessToken = response.extract().path("accessToken");
+            userRequest.delete(accessToken);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         driver.quit();
     }
 }
